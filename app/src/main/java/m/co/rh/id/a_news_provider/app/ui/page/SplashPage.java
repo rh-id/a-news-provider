145c01afd4605306252c0c0dac2d1a3b43fd5552
Ruby Hartono <58564005+rh-id@users.noreply.github.com>
2021-10-20 09:03:37 +0700

    [Refactor] Move SplashPage initialization logic

--- app/src/main/java/m/co/rh/id/a_news_provider/app/ui/page/SplashPage.java
+++ app/src/main/java/m/co/rh/id/a_news_provider/app/ui/page/SplashPage.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import m.co.rh.id.a_news_provider.R;
-import m.co.rh.id.a_news_provider.app.provider.AppNotificationHandler;
 import m.co.rh.id.a_news_provider.base.BaseApplication;
 import m.co.rh.id.anavigator.StatefulView;
 import m.co.rh.id.anavigator.component.INavigator;
@@ -15,7 +14,6 @@ import m.co.rh.id.aprovider.Provider;
 
 public class SplashPage extends StatefulView<Activity> implements RequireNavigator {
     private transient INavigator mNavigator;
-    private boolean mInitialized;
 
     @Override
     public void provideNavigator(INavigator navigator) {
@@ -23,14 +21,16 @@ public class SplashPage extends StatefulView<Activity> implements RequireNavigat
     }
 
     @Override
-    protected View createView(Activity activity, ViewGroup container) {
+    protected void initState(Activity activity) {
+        super.initState(activity);
         Provider provider = BaseApplication.of(activity).getProvider();
-        if(!mInitialized){
-            provider.get(Handler.class)
-                    .postDelayed(() ->
-                            mNavigator.retry(new HomePage()), 1000);
-            mInitialized = true;
-        }
+        provider.get(Handler.class)
+                .postDelayed(() ->
+                        mNavigator.retry(new HomePage()), 1000);
+    }
+
+    @Override
+    protected View createView(Activity activity, ViewGroup container) {
         return activity.getLayoutInflater().inflate(R.layout.page_splash, container, false);
     }
 
