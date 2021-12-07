6ac442426a9e5d45a803e9edfcb8a65ae50ea6d0
Ruby Hartono <58564005+rh-id@users.noreply.github.com>
2021-12-07 14:19:48 +0700

    [Test] Fix instrumented test work manager

--- app/src/androidTest/java/m/co/rh/id/a_news_provider/test/TestApplication.java
+++ app/src/androidTest/java/m/co/rh/id/a_news_provider/test/TestApplication.java
@@ -2,11 +2,17 @@ package m.co.rh.id.a_news_provider.test;
 
 import android.app.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.work.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+
 import m.co.rh.id.a_news_provider.base.BaseApplication;
 import m.co.rh.id.anavigator.component.INavigator;
 import m.co.rh.id.aprovider.Provider;
 
-public class TestApplication extends BaseApplication {
+public class TestApplication extends BaseApplication implements Configuration.Provider {
 
     private Provider mProvider;
 
@@ -22,5 +28,17 @@ public class TestApplication extends BaseApplication {
     @Override
     public INavigator getNavigator(Activity activity) {
         return mProvider.get(INavigator.class);
+    }
+
+    @NonNull
+    @Override
+    public Configuration getWorkManagerConfiguration() {
+        ExecutorService executorService = mProvider.get(ScheduledExecutorService.class);
+
+        return new Configuration.Builder()
+                .setMinimumLoggingLevel(android.util.Log.INFO)
+                .setExecutor(executorService)
+                .setTaskExecutor(executorService)
+                .build();
     }
 }
