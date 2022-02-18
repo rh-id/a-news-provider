032c9a179be55eed9a496333f4df16a185c5c645
Ruby Hartono <58564005+rh-id@users.noreply.github.com>
2022-02-18 14:14:56 +0700

    [InstrumentedTest] Fix integration test setup

--- app/src/androidTest/java/m/co/rh/id/a_news_provider/provider/IntegrationTestAppProviderModule.java
+++ app/src/androidTest/java/m/co/rh/id/a_news_provider/provider/IntegrationTestAppProviderModule.java
@@ -8,9 +8,9 @@ import androidx.work.WorkManager;
 import m.co.rh.id.a_news_provider.app.component.AppNotificationHandler;
 import m.co.rh.id.a_news_provider.app.provider.CommandProviderModule;
 import m.co.rh.id.a_news_provider.app.provider.StatefulViewProvider;
+import m.co.rh.id.a_news_provider.app.provider.event.AppSharedPreferencesEventHandler;
 import m.co.rh.id.a_news_provider.app.provider.notifier.RssChangeNotifier;
 import m.co.rh.id.a_news_provider.app.provider.parser.OpmlParser;
-import m.co.rh.id.a_news_provider.base.AppSharedPreferences;
 import m.co.rh.id.a_news_provider.base.provider.BaseProviderModule;
 import m.co.rh.id.a_news_provider.base.provider.DatabaseProviderModule;
 import m.co.rh.id.a_news_provider.component.network.provider.NetworkProviderModule;
@@ -42,8 +42,8 @@ public class IntegrationTestAppProviderModule implements ProviderModule {
 
         providerRegistry.registerLazy(AppNotificationHandler.class, () -> new AppNotificationHandler(provider, context));
         providerRegistry.registerLazy(WorkManager.class, () -> WorkManager.getInstance(context));
+        providerRegistry.registerLazy(AppSharedPreferencesEventHandler.class, () -> new AppSharedPreferencesEventHandler(provider, context));
         // for rss
-        providerRegistry.registerLazy(AppSharedPreferences.class, () -> new AppSharedPreferences(provider, context));
         providerRegistry.registerLazy(RssChangeNotifier.class, () -> new RssChangeNotifier(provider, context));
         providerRegistry.registerLazy(OpmlParser.class, () -> new OpmlParser(provider, context));
 
